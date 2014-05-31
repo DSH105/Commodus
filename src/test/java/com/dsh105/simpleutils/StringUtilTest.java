5bd1f1ac82d40f3525d98cc6d9163b49e9424692
Luke Anderson (stuntguy3000) <stuntguy3000@gmail.com>
2014-05-31 20:48:20 +0930

    Add Maven Testing

---
+++ src/test/java/com/dsh105/simpleutils/StringUtilTest.java
@@ -0,0 +1,23 @@
+package com.dsh105.simpleutils;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class StringUtilTest {
+    @Test
+    public void testCapitalization() {
+        Assert.assertEquals("Test Message", StringUtil.capitalise("test message"));
+    }
+
+    @Test
+    public void testSentenceBuilder() {
+        Assert.assertEquals("one, two and three", StringUtil.buildSentenceList("one", "two", "three"));
+        Assert.assertEquals("one and two", StringUtil.buildSentenceList("one", "two"));
+    }
+
+    @Test
+    public void testNumericExtraction() {
+        Assert.assertEquals(100, GeneralUtil.numericValueOf("100"));
+        Assert.assertEquals(100, GeneralUtil.numericValueOf("Example Number (100)"));
+    }
+}
