3069c58db64dc7820a2dbf63e239870687644280
DSH105 <jason.chirgwin@icloud.com>
2014-07-09 20:44:17 +1000

    Rename some things

--- src/main/java/com/dsh105/commodus/dependency/PluginDependencyProvider.java
+++ src/main/java/com/dsh105/commodus/dependency/PluginDependencyProvider.java
@@ -17,104 +17,15 @@
 
 package com.dsh105.commodus.dependency;
 
-import org.bukkit.Bukkit;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.server.PluginDisableEvent;
-import org.bukkit.event.server.PluginEnableEvent;
 import org.bukkit.plugin.Plugin;
 
-/**
- * Needs some optimization
- */
-public abstract class PluginDependencyProvider<T extends Plugin> implements IPluginDependencyProvider<T> {
+public interface PluginDependencyProvider<T extends Plugin> {
 
-    protected PluginDependencyProvider<T> instance;
-    private T dependency;
-    protected boolean hooked;
-    private Plugin myPluginInstance;
-    private String dependencyName;
+    public T getDependency();
 
-    // TODO: add more utils, plugin stuff mostly.
+    public boolean isHooked();
 
-    public PluginDependencyProvider(Plugin myPluginInstance, String dependencyName) {
-        this.instance = this;
-        this.myPluginInstance = myPluginInstance;
-        this.dependencyName = dependencyName;
+    public Plugin getHandlingPlugin();
 
-        if (dependency == null && !this.hooked) {
-            try {
-                dependency = (T) Bukkit.getPluginManager().getPlugin(getDependencyName());
-
-                if (this.dependency != null && this.dependency.isEnabled()) {
-                    this.hooked = true;
-                    onHook();
-                    //EchoPet.getPlugin().getModuleLogger().info("[" + this.dependency.getName() + "] Successfully hooked");
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-                //EchoPet.getPlugin().getReflectionLogger().warning("Could not create a PluginDependencyProvider for: " + getDependencyName() + "! (Are you sure the type is valid?)");
-            }
-        }
-
-        Bukkit.getPluginManager().registerEvents(new Listener() {
-
-            @EventHandler
-            protected void onEnable(PluginEnableEvent event) {
-                if ((dependency == null) && (event.getPlugin().getName().equalsIgnoreCase(getDependencyName()))) {
-                    try {
-                        dependency = (T) event.getPlugin();
-                        hooked = true;
-                        onHook();
-                        //EchoPet.getPlugin().getModuleLogger().info("[" + getDependencyName() + "] Successfully hooked");
-                    } catch (Exception e) {
-                        throw new RuntimeException("Failed to hook plugin: " + event.getPlugin().getName());
-                    }
-                }
-            }
-
-            @EventHandler
-            protected void onDisable(PluginDisableEvent event) {
-                if ((dependency != null) && (event.getPlugin().getName().equalsIgnoreCase(getDependencyName()))) {
-                    dependency = null;
-                    hooked = false;
-                    onUnhook();
-                    //EchoPet.getPlugin().getModuleLogger().info("[" + getDependencyName() + "] Successfully unhooked");
-                }
-            }
-        }, getHandlingPlugin());
-    }
-
-    public abstract void onHook();
-
-    public abstract void onUnhook();
-
-    @Override
-    public T getDependency() {
-        if (this.dependency == null) {
-            throw new RuntimeException("Dependency is NULL!");
-        }
-        return this.dependency;
-    }
-
-    @Override
-    public boolean isHooked() {
-        return this.hooked;
-    }
-
-    @Override
-    public Plugin getHandlingPlugin() {
-        if (this.myPluginInstance == null) {
-            throw new RuntimeException("HandlingPlugin is NULL!");
-        }
-        return this.myPluginInstance;
-    }
-
-    @Override
-    public String getDependencyName() {
-        if (this.dependencyName == null) {
-            throw new RuntimeException("Dependency name is NULL!");
-        }
-        return this.dependencyName;
-    }
+    public String getDependencyName();
 }
