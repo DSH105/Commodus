425de21fd64c302959b633c37175159670b8d873
DSH105 <jason.chirgwin@icloud.com>
2014-12-30 09:53:27 +1100

    Implement Player and ItemStack containers for Bukkit/Sponge compatibility

---
+++ src/main/java/com/dsh105/commodus/container/ItemStackContainer.java
@@ -0,0 +1,83 @@
+/*
+ * This file is part of Commodus.
+ *
+ * Commodus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Commodus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Commodus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.dsh105.commodus.container;
+
+import com.dsh105.commodus.bukkit.ItemRegistry;
+import com.dsh105.commodus.bukkit.MaterialId;
+import com.dsh105.commodus.sponge.SpongeUtil;
+import org.bukkit.Material;
+import org.spongepowered.api.GameRegistry;
+
+public class ItemStackContainer extends Container<ItemStackContainer> {
+
+    // TODO: docs
+
+    private String id;
+    private int quantity;
+
+    private ItemStackContainer(String id, int quantity) {
+        this.id = id;
+        this.quantity = quantity;
+    }
+
+    public static ItemStackContainer of(String id, int quantity) {
+        return new ItemStackContainer(id, quantity);
+    }
+
+    public static ItemStackContainer of(MaterialId id, int quantity) {
+        return of(ItemRegistry.getName(id), quantity);
+    }
+
+    public static ItemStackContainer of(org.bukkit.Material bukkitMaterial, int quantity) {
+        return of(bukkitMaterial, 0, quantity);
+    }
+
+    public static ItemStackContainer of(Material bukkitMaterial, int materialData, int quantity) {
+        return of(ItemRegistry.getName(bukkitMaterial, materialData), quantity);
+    }
+
+    public static ItemStackContainer of(org.bukkit.inventory.ItemStack bukkitItemStack) {
+        return of(bukkitItemStack.getType(), bukkitItemStack.getDurability(), bukkitItemStack.getAmount());
+    }
+
+    public static ItemStackContainer of(org.spongepowered.api.item.ItemType spongeType, int quantity) {
+        return of(spongeType.getId(), quantity);
+    }
+
+    public static ItemStackContainer of(org.spongepowered.api.item.inventory.ItemStack spongeItemStack) {
+        return of(spongeItemStack.getItem(), spongeItemStack.getQuantity());
+    }
+
+    public org.bukkit.inventory.ItemStack asBukkit() {
+        MaterialId id = ItemRegistry.getId(this.id);
+        return new org.bukkit.inventory.ItemStack(id.getId(), quantity, (short) id.getData());
+    }
+
+    public org.spongepowered.api.item.inventory.ItemStack asSponge() {
+        GameRegistry gameRegistry = SpongeUtil.getGame().getRegistry();
+        return gameRegistry.getItemBuilder().withItemType(gameRegistry.getItem(id).get()).withQuantity(quantity).build();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+}
---
+++ src/main/java/com/dsh105/commodus/container/PlayerContainer.java
@@ -0,0 +1,90 @@
+/*
+ * This file is part of Commodus.
+ *
+ * Commodus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Commodus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Commodus.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.dsh105.commodus.container;
+
+import com.dsh105.commodus.ServerUtil;
+import com.dsh105.commodus.StringUtil;
+import com.dsh105.commodus.bukkit.BukkitIdentUtil;
+import com.dsh105.commodus.sponge.SpongeIdentUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class PlayerContainer extends Container<PlayerContainer> {
+
+    // TODO: docs
+
+    private static final Map<UUID, PlayerContainer> PLAYER_BRIDGES = new HashMap<>();
+
+    private UUID playerUID;
+
+    protected PlayerContainer(UUID playerUID) {
+        this.playerUID = playerUID;
+    }
+
+    public static PlayerContainer of(UUID playerUID) {
+        PlayerContainer bridge = PLAYER_BRIDGES.get(playerUID);
+        if (bridge == null) {
+            bridge = new PlayerContainer(playerUID);
+            PLAYER_BRIDGES.put(playerUID, bridge);
+        }
+        return bridge;
+    }
+
+    public static PlayerContainer of(org.bukkit.entity.Player bukkitPlayer) {
+        return of(StringUtil.convertUUID(BukkitIdentUtil.getIdent(bukkitPlayer)));
+    }
+
+    public static PlayerContainer of(org.spongepowered.api.entity.player.Player spongePlayer) {
+        return of(SpongeIdentUtil.getIdent(spongePlayer));
+    }
+
+    public UUID getUID() {
+        return playerUID;
+    }
+
+    public boolean is(UUID playerUID) {
+        return this.playerUID.equals(playerUID);
+    }
+
+    public boolean is(PlayerContainer player) {
+        return playerUID.equals(player.playerUID);
+    }
+
+    public Object get() {
+        switch (ServerUtil.getServerBrand()) {
+            case BUKKIT:
+            case SPIGOT:
+            case CAULDRON:
+                return BukkitIdentUtil.getPlayer(playerUID);
+            case SPONGE:
+                return SpongeIdentUtil.getPlayer(playerUID);
+            default:
+                return null;
+        }
+    }
+
+    public org.bukkit.entity.Player asBukkit() {
+        return BukkitIdentUtil.getPlayer(playerUID);
+    }
+
+    public org.spongepowered.api.entity.player.Player asSponge() {
+        return SpongeIdentUtil.getPlayer(playerUID);
+    }
+}
